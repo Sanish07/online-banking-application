bc5f2e3d52c65aa46b5a73df70cd79ce2b13560a
Sanish07 <sanishnirwan@gmail.com>
2024-12-13 20:19:21 +0530

    Added customer account creation REST functionality

--- accounts/src/main/java/com/sanish/accounts/controllers/AccountsController.java
+++ accounts/src/main/java/com/sanish/accounts/controllers/AccountsController.java
@@ -3,6 +3,8 @@ package com.sanish.accounts.controllers;
 import com.sanish.accounts.constants.AccountsConstants;
 import com.sanish.accounts.dto.CustomerDto;
 import com.sanish.accounts.dto.ResponseDto;
+import com.sanish.accounts.services.IAccountsService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 //Through produces attr. we are explicitly mentioning, this controller supports response type of JSON format
 public class AccountsController {
 
+    private IAccountsService accountsService;
+
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+        accountsService.createAccount(customerDto); //If everything went well, we will move on to next line
+        //Or we will be facing some business logic exception handled at global exception handler
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
--- accounts/src/main/java/com/sanish/accounts/services/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/sanish/accounts/services/impl/AccountsServiceImpl.java
@@ -1,11 +1,20 @@
 package com.sanish.accounts.services.impl;
 
+import com.sanish.accounts.constants.AccountsConstants;
 import com.sanish.accounts.dto.CustomerDto;
+import com.sanish.accounts.entities.Accounts;
+import com.sanish.accounts.entities.Customer;
+import com.sanish.accounts.exceptions.CustomerAlreadyExistsException;
+import com.sanish.accounts.mappers.CustomerMapper;
 import com.sanish.accounts.repositories.AccountsRepository;
 import com.sanish.accounts.repositories.CustomerRepository;
 import com.sanish.accounts.services.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
@@ -14,8 +23,46 @@ public class AccountsServiceImpl implements IAccountsService {
     private CustomerRepository customerRepository;
     private AccountsRepository accountsRepository;
 
+    /**
+     * @param customerDto - CustomerDto Object
+     */
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto,new Customer()); //Convert dto to pojo
 
+        //Check if the customer with provided mobile number already exists
+        Optional<Customer> fetchedCustomer = customerRepository.findByMobileNumber(customerDto.getMobileNumber());
+        if(fetchedCustomer.isPresent()){ //If already registered throw exception
+            throw new CustomerAlreadyExistsException("Customer already registered with mobile number : "+customerDto.getMobileNumber());
+        }
+
+        customer.setCreatedAt(LocalDateTime.now()); //Since created at, created by fields are not nullable
+        customer.setCreatedBy("Anonymous");
+
+        Customer savedCustomer = customerRepository.save(customer); //Save new customer
+
+        Accounts newAccount = createNewAccount(savedCustomer); //Create an account obj with saved customer details
+        accountsRepository.save(newAccount); //Save new account
     }
+
+    /**
+     * @param customer - Customer Object
+     * @return the new account details
+     */
+    private Accounts createNewAccount(Customer customer) { //Private account creation method
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccNumber = 1000000000L + new Random().nextInt(900000000);
+
+        newAccount.setAccountNumber(randomAccNumber);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedAt(LocalDateTime.now()); //Since created at, created by fields are not nullable
+        newAccount.setCreatedBy("Anonymous");
+
+
+        return newAccount;
+    }
+
+
 }
