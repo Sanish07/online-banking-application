8fe48200c74ba6b9b758499690953fbeccb5a282
Sanish07 <sanishnirwan@gmail.com>
2024-12-15 19:58:07 +0530

    Added input validations and more specific exceptions handled

--- accounts/src/main/java/com/sanish/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/sanish/accounts/dto/AccountsDto.java
@@ -1,14 +1,20 @@
 package com.sanish.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "Account number must not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be of 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account type must not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch address must not be null or empty")
     private String branchAddress;
 
 }
